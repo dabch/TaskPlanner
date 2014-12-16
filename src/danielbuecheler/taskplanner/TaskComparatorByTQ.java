43fe2de15d7c9e8c2bc252a6376ea47c9aea2b2f
Daniel Bücheler <dbahn25@t-online.de>
2014-12-16 17:02:54 +0100

    Added javadoc comment to TaskComparatorByTQ.java

--- src/danielbuecheler/taskplanner/TaskComparatorByTQ.java
+++ src/danielbuecheler/taskplanner/TaskComparatorByTQ.java
@@ -2,8 +2,15 @@ package danielbuecheler.taskplanner;
 
 import java.util.Comparator;
 
+/**
+ * This Comparator compares Tasks by their so-called TQ. If you ought to do t1
+ * before t2, the return value will be positive, if t2 should be done before t1
+ * it will be negative.
+ * 
+ * @author Daniel Bücheler / Marvin Heinzelmann
+ */
 public class TaskComparatorByTQ implements Comparator<Task> {
-	
+
 	// TODO update when Task gets additional attributes
 	@Override
 	public int compare(Task t1, Task t2) {
